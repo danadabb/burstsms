242aacd39b04f60f23ca3dbd6c6917d3ad2185e2
Dabbagh <dana.dabbagh@cadreon.com>
2017-12-05 04:23:24 +0000

    Fixed bug where plain message was being returned instead of a resolved promise in shortenUrls function in the instance where no URLS were present

--- src/controller/sms.js
+++ src/controller/sms.js
@@ -10,12 +10,12 @@ function bitlyShortener(url){
 function shortenUrls(message){
 	var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
 	var regex = new RegExp(expression);
-
 	var urls = message.match(regex);
-	
-	if(!urls) { return message };
+
+	if(!urls) { return Promise.resolve(message) };
 
 	var shortenedUrls = [];
+
 	urls.forEach(url => shortenedUrls.push(bitlyShortener(url)));
 
 	return Promise.all(shortenedUrls).then(bitlyUrls => 
