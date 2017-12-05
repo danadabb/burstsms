f94ead5141156e92c9397af778c5e7e48eef90b4
Dabbagh <dana.dabbagh@cadreon.com>
2017-12-05 04:25:57 +0000

    Integrated sms controller with sms router

--- src/routes/sms.js
+++ src/routes/sms.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var SMS = require('../controller/sms.js');
 
 router.post('/sms', function(req, res){
-	res.send("Recieved");
+	SMS.sendMessage(req.body.number, req.body.message)
+	.then(response => res.send(response))
+	.catch(error => res.send(error));
 });
 
 module.exports = router;
